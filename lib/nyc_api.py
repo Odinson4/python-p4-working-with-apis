da5341c60a8dc1cfde9f9486215e0688b68236c9
Dylan Abbott <dylanabbott33@yahoo.com>
2023-04-03 15:10:20 -0600

    complete lab

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+
 class GetPrograms:
     def __init__(self):
         pass
@@ -12,13 +13,14 @@ class GetPrograms:
         return response.content
 
     def program_school(self):
-    # we use the JSON library to parse the API response into nicely formatted JSON
+        # we use the JSON library to parse the API response into nicely formatted JSON
         programs_list = []
         programs = json.loads(self.get_programs())
         for program in programs:
             programs_list.append(program["agency"])
 
         return programs_list
+
 
 programs = GetPrograms()
 programs_schools = programs.program_school()
